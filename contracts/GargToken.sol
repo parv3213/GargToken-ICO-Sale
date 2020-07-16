2a08aa2a6a9684c960037ba859301c319247cfb8
parv3213 <parv3213@gmail.com>
2020-07-16 19:08:32 +0530

    delegated transfer

--- contracts/GargToken.sol
+++ contracts/GargToken.sol
@@ -12,7 +12,14 @@ contract GargToken {
         uint _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         // allocate the initial supply
@@ -28,4 +35,25 @@ contract GargToken {
         emit Transfer(msg.sender, _to,_value);
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[_from] >= _value,"_from does not have enough tokens");
+        require(allowance[_from][msg.sender] >= _value,"Spender limit exceeded");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    // transferFrom
+    // transfer event
+
+    
 }
