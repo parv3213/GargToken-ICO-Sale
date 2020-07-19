2a71e7bb691d8633ef80c559739f847669cc2c97
parv3213 <parv3213@gmail.com>
2020-07-19 11:54:09 +0530

    restructure

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,21 @@
+require("babel-register");
+require("babel-polyfill");
+
 module.exports = {
 	networks: {
 		development: {
 			host: "127.0.0.1",
-			port: "7545",
-			network_id: "*", //match any network id
+			port: 7545,
+			network_id: "*", // Match any network id
 		},
-		compilers: {
-			solc: {
-				version: "0.5.0",
+	},
+	contracts_directory: "./src/contracts/",
+	contracts_build_directory: "./src/abis/",
+	compilers: {
+		solc: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
 			},
 		},
 	},
